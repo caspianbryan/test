38ce386df79e7daf9458edab24d1b6b4ea8628e6
caspianbryan <brianberrieron@gmail.com>
2024-01-15 16:29:26 +0300

    test commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hellow')
+})
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT} successfully`);
+})
