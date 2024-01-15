8aeab02e0b1675d07fe088d6f5d4eab2d69034c8
caspianbryan <brianberrieron@gmail.com>
2024-01-15 16:35:57 +0300

    second changes

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hellow')
 })
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+      },
+]
 
 
 const PORT = process.env.PORT || 3001
